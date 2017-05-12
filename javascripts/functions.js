0e2bef9811b4d5cdf5e2b47d902011258b0d12e8
Dylan Howard <dylanhoward6@gmail.com>
2017-05-12 13:06:10 -0500

    - Minor changes and file organization modification

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -24,14 +24,6 @@ $(function() {
 });
 
 $(document).ready(function() {
-
-  $(function(){
-    $('.typed').typed({
-      strings: ['a portrait?', 'a family photo?', 'senior pictures?', 'prom photos?', 'a stock photo?', 'a website?', 'anything creative?'],
-      typeSpeed: 0
-    });
-  });
-
   /* On Ready */
   $('.ready-fade-object').ready( function() {
     setTimeout(function() {
