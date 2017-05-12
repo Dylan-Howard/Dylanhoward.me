58bf61dee940ba9f262010718fbf687a57460603
Dylan Howard <dylanhoward6@gmail.com>
2017-05-12 14:16:29 -0500

    - Minified JS and HTML

--- min/javascripts/typedInit.js
+++ min/javascripts/typedInit.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-  /* Typed Function */
-  $(function(){
-    $('.typed').typed({
-      strings: ['a portrait?', 'a family photo?', 'senior pictures?', 'prom photos?', 'a stock photo?', 'a website?', 'anything creative?'],
-      typeSpeed: 0
-    });
-  });
-})
+$(document).ready(function(){$(function(){$(".typed").typed({strings:"a portrait?;a family photo?;senior pictures?;prom photos?;a stock photo?;a website?;anything creative?".split(";"),typeSpeed:0})})});
