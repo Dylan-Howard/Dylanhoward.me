f023b2c85a630b831c5811fb7ba2d7ff0c8a48e9
Dylan Howard <dylanhoward6@gmail.com>
2017-05-17 10:47:13 -0500

    - Removed unecessary css from index.html. - Removed some photos from the portrait photography page. - Removed commented-out code from the function.js file. - Reorganized function.js file to not implement the carousel in mobile. - Removed lazy-loading files from the project.

--- pro/javascripts/functions.js
+++ pro/javascripts/functions.js
@@ -1,30 +1,31 @@
-/* Navbar Transitions*/
-$(window).scroll(function() {
-  if ($(document).scrollTop() > $(window).height()-35) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
-});
+$(document).ready(function() {
 
-/* Smooth Scrolling */
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+  /* Navbar Transitions*/
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > $(window).height()-35) {
+      $('nav').addClass('shrink');
+    } else {
+      $('nav').removeClass('shrink');
     }
   });
-});
 
-$(document).ready(function() {
-  /* On Ready */
+  /* Smooth Scrolling */
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  /* Fade-in */
   $('.ready-fade-object').ready( function() {
     setTimeout(function() {
       $('.ready-fade').css('top','0px');
@@ -34,24 +35,27 @@ $(document).ready(function() {
 
   /* Image Loading Control */
 
-  /* Main Container BG transition */
+  /* Main Container Carousel */
   $('#carousel-item2').ready( function() {
-    (function() {
-        var imgId = 0;
-        var numOfImages = 4; // Change this to the number of background images
-        window.setInterval(function() {
-            $('#carousel-item' + imgId).addClass('fade');
-            imgId = (imgId+1) % numOfImages;
-            $('#carousel-item' + imgId).removeClass('fade');
-        }, 10 * 1000);
-    })();
+
+    if($(window).width() > 1099) {
+      for(var i = 0; i < 4; i++) {
+        $('#carousel-item' + i).css('left', '');
+      }
+      (function() {
+          var imgId = 0;
+          var numOfImages = 4; // Change this to the number of background images
+          window.setInterval(function() {
+              $('#carousel-item' + imgId).addClass('fade');
+              imgId = (imgId+1) % numOfImages;
+              $('#carousel-item' + imgId).removeClass('fade');
+          }, 10 * 1000);
+      })();
+    }
   });
 
-
-  /* Lazy Load */
-  // $('.img-item').lazyLoadXT();
-
   /* Mobile Animations */
+
   /* Mobile Nav-bar */
   $('#expand-btn').click(function() {
     if($('nav').hasClass('expand')) {
@@ -75,10 +79,4 @@ $(document).ready(function() {
       $('nav').removeClass('expand');
     }
   });
-  /* Other Mobile */
-  if($(window).width() > 1099) {
-    for(var i = 0; i < 4; i++) {
-      $('#carousel-item' + i).css('left', '');
-    }
-  }
 });
