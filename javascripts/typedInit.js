18348701b525b06be3f0634641e87d40ba256cbb
Dylan Howard <dylanhoward6@gmail.com>
2017-06-15 10:55:08 -0500

    - No idea

---
+++ javascripts/typedInit.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  /* Typed Function */
+  $(function(){
+    $('.typed').typed({
+      strings: ['a portrait?', 'a family photo?', 'senior pictures?', 'prom photos?', 'a stock photo?', 'a website?', 'anything creative?'],
+      typeSpeed: 0
+    });
+  });
+})
